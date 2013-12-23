0cb60a9568d6ca5f9bbc3d324a675f32bf530487
Hugh <gamehaxe@gmail.com>
2013-12-23 22:27:51 +0800

    Get building on linux

--- build/setup/setup.h
+++ build/setup/setup.h
@@ -4,6 +4,12 @@
 #include "setup_hx_rpi.h"
 #elif defined(HX_MACOS)
 #include "setup_hx_mac.h"
+#elif defined(HX_LINUX)
+   #ifdef HXCPP_M64
+   #include "setup_hx_linux64.h"
+   #else
+   #include "setup_hx_linux.h"
+   #endif
 #else
 #error "Unknown platform"
 #endif
